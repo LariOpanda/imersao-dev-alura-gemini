d0c17baaaec46dc9ba1fa710ba5cd21f794990a1
LariOpanda <larissamayara.ro.@gmail.com>
2024-09-06 10:22:26 -0300

    first commit

---
+++ dados.js
@@ -0,0 +1,37 @@
+let filmesTimBurton = [
+    {
+      titulo: "A Fantástica Fábrica de Chocolate",
+      descricao: "Uma adaptação moderna do clássico de Roald Dahl, dirigida por Tim Burton.",
+      link: "https://www.imdb.com/title/tt0367540/"
+    },
+    {
+      titulo: "Edward Mãos de Tesoura",
+      descricao: "Um conto de fadas gótico sobre um homem artificial com mãos de tesoura.",
+      link: "https://www.imdb.com/title/tt0095016/"
+    },
+    {
+      titulo: "O Estranho Mundo de Jack",
+      descricao: "Jack Skellington, o Rei do Natal, descobre a festa do Halloween e decide trazê-la para o Mundo Natalino.",
+      link: "https://www.imdb.com/title/tt0116629/"
+    },
+    {
+      titulo: "Planeta dos Macacos (Versão de Tim Burton)",
+      descricao: "Uma nova versão do clássico filme de ficção científica, com Mark Wahlberg no papel principal.",
+      link: "https://www.imdb.com/title/tt0180022/"
+    },
+    {
+      titulo: "A Noiva Cadáver",
+      descricao: "Uma animação em stop-motion sobre um jovem que acidentalmente pede a mão de uma noiva morta em casamento.",
+      link: "https://www.imdb.com/title/tt0347149/"
+    },
+    {
+      titulo: "Vincent",
+      descricao: "Um curta-metragem em stop-motion, onde um menino imagina a si mesmo como Vincent Price, um ator de filmes de terror.",
+      link: "https://www.imdb.com/title/tt0103055/"
+    },
+    {
+      titulo: "Beetlejuice",
+      descricao: "Uma comédia de terror sobre um casal de fantasmas que contrata um bio-exorcista para espantar uma família que se mudou para sua antiga casa.",
+      link: "https://www.imdb.com/title/tt0091044/"
+    }
+  ];
